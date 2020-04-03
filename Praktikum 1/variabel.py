b42a8b375012ca57d99257ac7404e8f7b9eb1b9f
aenal-abie <aenal.abie@gmail.com>
2020-04-03 01:21:18 -0400

    contoh pmb dalam perulangan

---
+++ Praktikum 1/variabel.py
@@ -0,0 +1,12 @@
+nilai = 80 # int
+jumlah = 80 # int
+print(80) # int
+print(jumlah) # int
+
+kalimat = "Apa itu python" # string
+kalimat = 'Apa itu python'
+print(kalimat)
+
+persentase = 0.87 #
+print(persentase)
+
