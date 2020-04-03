613b2969ab0fead1106fde7ad0c970a6dd952461
aenal-abie <aenal.abie@gmail.com>
2020-04-02 22:56:18 -0400

    pembahasan praktikikum 3

---
+++ Praktikum 3/for_ganjil_genap.py
@@ -0,0 +1,6 @@
+for ulang in range(15,301,100):
+    if(ulang % 2==0):
+            ket = "Genap"
+    else:
+        ket = "Ganjil"
+    print(ket)
---
+++ Praktikum 3/ulang_ganjil_genap.py
@@ -0,0 +1,8 @@
+ulang = 15
+while(ulang<=300):
+    if(ulang % 2==0):
+        ket = "Genap"
+    else:
+        ket = "Ganjil"
+    ulang =  ulang + 1 # ulang +=1
+    print(ket)
