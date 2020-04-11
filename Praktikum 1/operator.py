da4fadcba4ec7a91dde8e285b28d75e5dd6950e2
aenal-abie <aenal.abie@gmail.com>
2020-04-10 23:00:56 -0400

    ll

---
+++ Praktikum 1/operator.py
@@ -0,0 +1,10 @@
+nilai1 =10
+nilai2 = 20
+
+print(nilai1+nilai2)
+print(nilai1-nilai2)
+print(nilai1*nilai2)
+print(nilai1/nilai2)
+print(nilai1**nilai2)
+print(nilai1%nilai2)
+print(nilai1//nilai2)
